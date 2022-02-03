f0e7d03842648980a2132add0aaeca6061fd5641
JGabrielNeto <joao.gabrielsn1997@gmail.com>
2022-02-03 14:48:39 -0300

    Dockerizing with docker-compose

---
+++ .docker/node/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ ! -f /usr/local/bin/yarn ]; then
+  echo "Install yarn"
+  npm i -g yarn
+fi
+
+if [ ! -d node_modules ]; then
+  echo "Install packages"
+  yarn
+fi
+echo "Done"
+
+if [ ! -d .next ]; then
+  echo "Building"
+  yarn build
+fi
+echo "Done"
+
+yarn dev
