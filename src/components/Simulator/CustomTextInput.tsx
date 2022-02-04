a7f273e26f93da15f77c80f94056bf83e925badb
JGabrielNeto <joao.gabrielsn1997@gmail.com>
2022-02-04 18:05:09 -0300

    Adding form validation

--- src/components/Simulator/CustomTextInput.tsx
+++ src/components/Simulator/CustomTextInput.tsx
@@ -1,15 +1,94 @@
 import React from "react";
 import styles from "../../styles/CustomTextInput.module.sass";
+import * as yup from 'yup'
+import { VALID_LOADERS } from "next/dist/server/image-config";
 
 const CustomTextInput: React.FC = (props) => {
+
+  const inputSchema = yup.object().shape({
+    initialContribution: yup.number(),
+    monthlyContribution: yup.number(),
+    deadline: yup.number(),
+    profitability: yup.number(),
+    ipca: yup.number().equals([10.06]),
+    cdi: yup.number().equals([9.15]),
+  })
+
+  const validateForm = async (event: any) => {
+    event.preventDefault();
+    let formData: object = {
+      initialContribution: +event.target[0].value,
+      monthlyContribution: +event.target[1].value,
+      deadline: +event.target[2].value,
+      profitability: +event.target[3].value,
+      ipca: +event.target[4].value,
+      cdi: +event.target[5].value
+    };
+    
+    const dataValidation = await inputSchema.isValid(formData)
+    console.log(dataValidation)
+  }
+
   return (
+  <>
+  <form className={styles.form} id="form" onSubmit={validateForm}>
     <div className={styles.container}>
-      <label htmlFor="inp" className={styles.inp}>
-        <input type="text" id="inp" placeholder="&nbsp;" />
-        <span className={styles.label}>{props.children}</span>
-        <span className={styles.focusBg}></span>
+      <label className={styles.inp}>
+        <input id="inicial" type="text" placeholder="" pattern="^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$"/>
+        <span className={styles.label}>Aporte Inicial</span>
+        <span className={styles.focusBg}/>
+        <span className={styles.warning}>Aporte deve ser um número.</span>
       </label>
     </div>
+
+    <div className={styles.container}>
+      <label className={styles.inp}>
+        <input id="mensal" type="text" placeholder="" pattern="^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$"/>
+        <span className={styles.label}>Aporte Mensal</span>
+        <span className={styles.focusBg}/>
+        <span className={styles.warning}>Aporte deve ser um número.</span>
+      </label>
+    </div>
+
+    <div className={styles.container}>
+      <label className={styles.inp}>
+        <input id="prazo" type="text" placeholder="" pattern="^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$"/>
+        <span className={styles.label}>Prazo (em meses)</span>
+        <span className={styles.focusBg}/>
+        <span className={styles.warning}>Insira um <u>número</u> de meses válido</span>
+      </label>
+    </div>
+
+    <div className={styles.container}>
+      <label className={styles.inp}>
+        <input id="rentabilidade" type="text" placeholder="" pattern="^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$"/>
+        <span className={styles.label}>Rentabilidade</span>
+        <span className={styles.focusBg}/>
+        <span className={styles.warning}>Rentabilidade deve ser um número</span>
+      </label>
+    </div>
+
+    <div className={styles.container}>
+      <label className={styles.inp}>
+        <input required readOnly type="text" placeholder="" value="10.06%"/>
+        <span className={styles.label}>IPCA (ao ano)</span>
+        <span className={styles.focusBg}/>
+        </label>
+    </div>
+
+    <div className={styles.container}>
+      <label className={styles.inp}>
+        <input required readOnly type="text" placeholder="" value="9.15%"/>
+        <span className={styles.label}>CDI (ao ano)</span>
+        <span className={styles.focusBg}/>
+      </label>
+    </div>
+
+      <button className={styles.btn}><b>Limpar campos</b></button>
+      <button type="submit" form="form" className={[styles.btn, styles.submitBtn].join(" ")}><b>Simular</b></button>
+  </form>
+  </>
+
   )
 }
 
--- src/components/Simulator/Simulator.tsx
+++ src/components/Simulator/Simulator.tsx
@@ -4,20 +4,15 @@ import CustomRadioIncome from "./CustomRadioIncome";
 import CustomRadioIndexing from "./CustomRadioIndexing";
 import CustomTextInput from "./CustomTextInput";
 
+
 const Simulator: React.FC = () => {
+
   return (
     <div className={styles.container}>
         <h2>Simulador</h2>
         <CustomRadioIncome/>
         <CustomRadioIndexing/>
-        <CustomTextInput>Aporte inicial</CustomTextInput>
-        <CustomTextInput>Aporte mensal</CustomTextInput>
-        <CustomTextInput>Prazo (em meses)</CustomTextInput>
-        <CustomTextInput>Rentabilidade</CustomTextInput>
-        <CustomTextInput>IPCA(ao ano)</CustomTextInput>
-        <CustomTextInput>CDI (ao ano)</CustomTextInput>
-        <button className={styles.btn}><b>Limpar campos</b></button>
-        <button className={[styles.btn, styles.submitBtn].join(" ")}><b>Simular</b></button>
+        <CustomTextInput/>
     </div>
       
   )
